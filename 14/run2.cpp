26320c759ea59bab3768a228f1e68d63fa8e08b3
Armin Samii <armin@avametric.com>
2018-12-14 00:38:53 -0800

    OH! (h/t reddit thread)

--- 14/run2.cpp
+++ 14/run2.cpp
@@ -2,6 +2,13 @@
 #include <cstdlib>
 #include <vector>
 
+inline bool chk(const std::vector<char>& rec) {
+    unsigned long long int size = rec.size();
+    bool is = rec[size-1] == 1 && rec[size-2] == 4 && rec[size-3] == 6 && rec[size-4] == 0 && rec[size-5] == 7 && rec[size-6] == 1;
+    if(is) std::cout << rec.size()-6 << std::endl;
+    return is;
+}
+
 int main() {
 std::vector<char> rec;
 rec.push_back(3);
@@ -14,15 +21,16 @@ unsigned long long int tic = 0;
 unsigned long long int size = 0;
 while(true) {
     char v = rec[elf0] + rec[elf1];
-    if(v >= 10)
+    if(v >= 10) {
         rec.push_back(v/10);
+        if(chk(rec)) break;
+    }
     rec.push_back(v % 10);
 
     size = rec.size();
     elf0 = (elf0 + rec[elf0] + 1) % size;
     elf1 = (elf1 + rec[elf1] + 1) % size;
-    if(rec[size-1] == 1 && rec[size-2] == 4 && rec[size-3] == 6 && rec[size-4] == 0 && rec[size-5] == 7 && rec[size-6] == 1) {
-        std::cout << rec.size()-6 << std::endl;
+    if(chk(rec)) {
         break;
     }
     tic++;
